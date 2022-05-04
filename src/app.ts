7efb08057ef7334abde7889f5c250a3683b7c24b
JP3tri3 <JP3tri3@gmail.com>
2022-05-04 13:46:29 -0400

    init commit

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('nooooooope');
+});
+
+app.listen(8080, () => {
+    console.log('I can hear you...');
+});
