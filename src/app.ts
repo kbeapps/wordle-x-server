85a871a2c84f3ecd1fc91b2ad895ca1b3fe9c282
JP3tri3 <JP3tri3@gmail.com>
2022-05-05 14:20:13 -0400

    added .env & dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
 
+dotenv.config();
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('nooooooope');
+    return res.send('The horses are out of the barn.');
 });
 
-app.listen(8080, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
     console.log('I can hear you...');
 });
-
-console.log('test')
