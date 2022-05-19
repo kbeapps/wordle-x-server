b2d6bd58d8d4a4025cf033cf5236a7d6c90054d8
JP3tri3 <JP3tri3@gmail.com>
2022-05-18 20:51:05 -0400

    created Game model

---
+++ src/models/game.model.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+export interface IGame {
+    name: string;
+    players: string[];
+    wordHistory: string[];
+    boards?: object[];
+    type: string,
+    winCondition: string,
+    wordSize: number,
+    theme?: string,
+};
+
+const GameSchema = new Schema<IGame>({
+    name: { type: String, required: true, unique: true, minLength: 2, maxLength: 16 },
+    players: { type: [String], required: true, min: 1, max: 20 },
+    wordHistory: { type: [String], required: true, min: 1, max: 365 },
+    boards: { type: [Object] },
+    type: { type: String, required: true, min: 1, max: 20 },
+    winCondition: { type: String, required: true, min: 1, max: 20 },
+    wordSize: { type: Number, required: true, min: 4, max: 10 },
+    theme: { type: String, min: 1, max: 20 },
+}, { timestamps: true });
+
+const Game = mongoose.model<IGame>('Game', GameSchema);
+
+export default Game;
