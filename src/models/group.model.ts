daee3ea0d82b57857e48b5d54d8b2adce3374269
lw3b3r <daevahush@gmail.com>
2022-05-18 20:29:53 -0400

    Created Group model

---
+++ src/models/group.model.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+export interface IGroup {
+    groupName: string;
+    members: string[];
+};
+
+const GroupSchema = new Schema<IGroup>({
+    groupName: { type: String, required: true, minLength: 5, maxLength: 16 },
+    members: { type: [String], required: true, min: 2, max: 20 }
+});
+
+const Group = mongoose.model<IGroup>('Group', GroupSchema);
+
+export default Group;
