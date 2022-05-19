a05026002c720746d027e6e089da4caa7210d308
lw3b3r <daevahush@gmail.com>
2022-05-18 20:46:51 -0400

    Created Notification model. Added timestamps to Group model.

--- src/models/group.model.ts
+++ src/models/group.model.ts
@@ -9,7 +9,7 @@ export interface IGroup {
 const GroupSchema = new Schema<IGroup>({
     groupName: { type: String, required: true, minLength: 5, maxLength: 16 },
     members: { type: [String], required: true, min: 2, max: 20 }
-});
+}, { timestamps: true });
 
 const Group = mongoose.model<IGroup>('Group', GroupSchema);
 
---
+++ src/models/notification.model.ts
@@ -0,0 +1,15 @@
+import mongoose, { ObjectId } from 'mongoose';
+const { Schema } = mongoose;
+
+export interface INotification {
+    userId: string,
+    message: string
+};
+
+const NotificationSchema = new Schema<INotification>({
+    userId: { type: String, required: true },
+    message: { type: String, required: true } }, { timestamps: true });
+
+    const Notification = mongoose.model<INotification>('Notification', NotificationSchema);
+
+    export default Notification;
