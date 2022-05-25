dfe1f3ae4657a912c90ae743c799ae593ca180cd
JP3tri3 <JP3tri3@gmail.com>
2022-05-25 18:32:09 -0400

    created auth signin / signup middleware tests

--- test/middleware/auth.middleware.test.ts
+++ test/middleware/auth.middleware.test.ts
@@ -1,49 +1,80 @@
-const { signup } = require('../../src/middleware/auth.middleware');
 const router = require('../../src/routes');
 const request = require('supertest');
 import db from '../../src/models';
+import User from '../../src/models/user.model';
 
-// test('will return user', () => {
-//     const req = new Request({
-//         body: { email: 'test', username: 'test', password: 'test' }
-//     });
-//     expect(signup(req)).toBe('test');
-// });
+const testUser = {
+    email: 'test@test.com',
+    username: 'testUser',
+    password: 'test123'
+};
 
+beforeAll(async () => {
+    try {
+        const connected = await db.mongoose
+            .connect(db.url);
 
+        if (connected) {
+            console.log('Database connection successful.');
+        };
+    } catch (err) {
+        console.log('error: ', err);
+        process.exit();
+    };
+});
 
 describe('POST /auth/signup', () => {
-    
-    jest.setTimeout(30000);
     describe('given a username, email & password', () => {
-        // should check for duplicate username / email
-        // should create new user in database
         test('should respond with status 200', async () => {
-            try {
-                const connected = await db.mongoose
-                    .connect(db.url);
-        
-                if (connected) {
-                    console.log('Database connection successful.');
-                };
-            } catch (err) {
-                console.log('error: ', err);
-                process.exit();
-            };
-            
-            const res = await request(router).post('/auth/signup').send({
-                email: 'test@test.com',
-                username: 'testUser',
-                password: 'test123'
-            });
+            const res = await request(router).post('/auth/signup').send(testUser);
             expect(res.statusCode).toBe(200);
-        });
 
+        });
     });
 
     describe('something is missing', () => {
-        // should respond with status 400
+        test('should respond with status 400', async () => {
+            const res = await request(router).post('/auth/signup').send({
+                email: testUser.email,
+                username: testUser.username
+            });
+            expect(res.statusCode).toBe(400);
+        });
+    });
+});
 
+describe('POST /auth/signin', () => {
+    describe('given a email & password', () => {
+        test('should respond with status 200', async () => {
+            const res = await request(router).post('/auth/signin').send({
+                email: testUser.email,
+                password: testUser.password
+            });
+            expect(res.statusCode).toBe(200);
+        });
     });
 
+    describe('given a username & password', () => {
+        test('should respond with status 200', async () => {
+            const res = await request(router).post('/auth/signin').send({
+                username: testUser.username,
+                password: testUser.password
+            });
+            expect(res.statusCode).toBe(200);
+        });
+    });
+
+    describe('something is missing', () => {
+        test('should respond with status 400', async () => {
+            const res = await request(router).post('/auth/signin').send({
+                username: testUser.password
+            });
+            expect(res.statusCode).toBe(400);
+        });
+    });
+});
+
+afterAll(async () => {
+    await User.deleteOne({ email: 'test@test.com' });
+    await db.mongoose.connection.close();
 });
