d505830605d6c2178833be33c2a3894ca04d04ab
JP3tri3 <JP3tri3@gmail.com>
2022-05-30 18:36:43 -0400

    setup game controller

---
+++ src/controllers/game.controller.ts
@@ -0,0 +1,55 @@
+import Game from '../models/game.model';
+
+const get = async (query: object) => {
+    try {
+        return await Game.findOne(query);
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+const create = async (name: string, players: string[], wordHistory: string[], type: string, winCondition: string, wordSize: string, theme?: string) => {
+    const game = new Game({
+        name: name,
+        players: players,
+        wordHistory: wordHistory,
+        boards: [],
+        type: type,
+        winCondition: winCondition,
+        wordSize: wordSize,
+        theme: ''
+    });
+
+    if (theme) {
+        game.theme = theme;
+    };
+
+    try {
+        return await game.save();
+    } catch (error) {
+        console.log(error);
+        return error;
+    };
+};
+
+const update = async (_id: string, query: object) => {
+    try {
+        return await Game.findByIdAndUpdate(
+            _id, query, { new: true }
+        );
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+const remove = async (_id: string) => {
+    try {
+        return await Game.findByIdAndRemove(_id);
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+export default {
+    create, get, update, remove
+};
