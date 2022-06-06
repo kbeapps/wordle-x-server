697038a2c02f064232f00b12d6f62a2f9fe24998
JP3tri3 <JP3tri3@gmail.com>
2022-06-06 14:25:01 -0400

    updated models & controllers with model interfaces

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,6 @@
-import mongoose, { Schema, Types } from 'mongoose';
+import mongoose, { Document, Schema, Types } from 'mongoose';
 
-export interface IUser {
+export interface IUser extends Document {
     email: string;
     username: string;
     password: string;
