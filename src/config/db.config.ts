2f3b1362be83dfd3e7ac4ad03a649c38364cc344
JP3tri3 <JP3tri3@gmail.com>
2022-06-09 10:26:49 -0400

    refactor(env): NODE_ENV was unnecessary in .env and cleaned up db url declaration

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -2,8 +2,5 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  url:
-    process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'development'
-      ? process.env.LOCAL_DB_URL
-      : process.env.PROD_DB_URL,
+  url: process.env.NODE_ENV === 'production' ? process.env.PROD_DB_URL : process.env.LOCAL_DB_URL,
 };
