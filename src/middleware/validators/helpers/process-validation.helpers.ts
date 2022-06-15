f02c3db3831327f661fa9e9295d10028b11a4e49
lw3b3r <daevahush@gmail.com>
2022-06-15 19:31:23 -0400

    Updated username verification regex to exclude allowing special characters in usernames outside of . - and _

--- src/middleware/validators/helpers/process-validation.helpers.ts
+++ src/middleware/validators/helpers/process-validation.helpers.ts
@@ -1,197 +1,211 @@
-const EMAIL_VERIFICATION_REGEX = /^([a-zA-Z0-9-.]+)@([a-zA-Z0-9-.]+).([a-zA-Z]{2,5})$/;
+const EMAIL_VERIFICATION_REGEX =
+	/^([a-zA-Z0-9-.]+)@([a-zA-Z0-9-.]+).([a-zA-Z]{2,5})$/;
 const PASSWORD_VERIFICATION_REGEX = /^[a-zA-Z0-9_.!@$%&(){}:;<>,?+=|-]{5,20}$/;
 const ID_VERIFICATION_REGEX = /^[a-zA-Z0-9]{24,24}$/;
-const USERNAME_VERIFICATION_REGEX = /^[a-zA-Z0-9_.!@$%&(){}:;<>,?+=|-]{0,}$/;
+const USERNAME_VERIFICATION_REGEX = /^[a-zA-Z0-9_.-]{0,}$/;
 const DEFAULT_REGEX = /^[a-zA-Z0-9_.!@$%&(){}:;<>, ?+=|-]{0,}$/;
 
-const checkKeys = (bodyKeys: string[], requiredKeys: string[], allowedKeys?: string[]): boolean => {
-  const allKeys = allowedKeys ? [...requiredKeys, ...allowedKeys] : requiredKeys;
-  const hasRequiredKeys = requiredKeys.every((key) => bodyKeys.includes(key));
-  const allKeysValid = bodyKeys.every((key) => allKeys.includes(key));
+const checkKeys = (
+	bodyKeys: string[],
+	requiredKeys: string[],
+	allowedKeys?: string[]
+): boolean => {
+	const allKeys = allowedKeys
+		? [...requiredKeys, ...allowedKeys]
+		: requiredKeys;
+	const hasRequiredKeys = requiredKeys.every((key) => bodyKeys.includes(key));
+	const allKeysValid = bodyKeys.every((key) => allKeys.includes(key));
 
-  return hasRequiredKeys && allKeysValid;
+	return hasRequiredKeys && allKeysValid;
 };
 
 const checkAllowedValueTypes = (body: object): string => {
-  const stringIsValid = (strToValidate: string, key: string): boolean => {
-    let regex = /[]/;
+	const stringIsValid = (strToValidate: string, key: string): boolean => {
+		let regex = /[]/;
 
-    switch (key) {
-      case 'email': {
-        regex = EMAIL_VERIFICATION_REGEX;
-        break;
-      }
-      case 'password': {
-        regex = PASSWORD_VERIFICATION_REGEX;
-        break;
-      }
-      case '_id':
-      case 'ownerId': {
-        regex = ID_VERIFICATION_REGEX;
+		switch (key) {
+			case 'email': {
+				regex = EMAIL_VERIFICATION_REGEX;
+				break;
+			}
+			case 'password': {
+				regex = PASSWORD_VERIFICATION_REGEX;
+				break;
+			}
+			case '_id':
+			case 'ownerId': {
+				regex = ID_VERIFICATION_REGEX;
 
-        break;
-      }
-      case 'username': {
-        regex = USERNAME_VERIFICATION_REGEX;
-        break;
-      }
-      default: {
-        regex = DEFAULT_REGEX;
-        break;
-      }
-    }
-    return regex.test(strToValidate);
-  };
+				break;
+			}
+			case 'username': {
+				regex = USERNAME_VERIFICATION_REGEX;
+				break;
+			}
+			default: {
+				regex = DEFAULT_REGEX;
+				break;
+			}
+		}
+		return regex.test(strToValidate);
+	};
 
-  const getArrayErrors = (elementType: string, shouldBeArray: any): string | undefined => {
-    const isArray = Array.isArray(shouldBeArray);
+	const getArrayErrors = (
+		elementType: string,
+		shouldBeArray: any
+	): string | undefined => {
+		const isArray = Array.isArray(shouldBeArray);
 
-    if (!isArray) {
-      return `${typeof shouldBeArray}`;
-    }
+		if (!isArray) {
+			return `${typeof shouldBeArray}`;
+		}
 
-    let elemTypeIsValid: boolean = true;
-    let failedIdx: number = 0;
-    if (isArray) {
-      for (const [index, element] of shouldBeArray.entries()) {
-        if (typeof element !== elementType) {
-          failedIdx = index;
-          elemTypeIsValid = false;
-          break;
-        }
-      }
-    }
+		let elemTypeIsValid: boolean = true;
+		let failedIdx: number = 0;
+		if (isArray) {
+			for (const [index, element] of shouldBeArray.entries()) {
+				if (typeof element !== elementType) {
+					failedIdx = index;
+					elemTypeIsValid = false;
+					break;
+				}
+			}
+		}
 
-    return !elemTypeIsValid ? `${typeof shouldBeArray[failedIdx]}[]` : undefined;
-  };
+		return !elemTypeIsValid
+			? `${typeof shouldBeArray[failedIdx]}[]`
+			: undefined;
+	};
 
-  const createMessage = (
-    key: string,
-    requiredValueType: string,
-    receivedValueType: string
-  ): string => {
-    return `Invalid value type for key: ${key}. Expected type: ${requiredValueType}. Received type: ${receivedValueType}.`;
-  };
+	const createMessage = (
+		key: string,
+		requiredValueType: string,
+		receivedValueType: string
+	): string => {
+		return `Invalid value type for key: ${key}. Expected type: ${requiredValueType}. Received type: ${receivedValueType}.`;
+	};
 
-  const numberTypeKeys: string[] = ['wordSize'];
-  const objectArrayTypeKeys: string[] = ['boards'];
-  const stringArrayTypeKeys: string[] = [
-    'players',
-    'wordHistory',
-    'members',
-    'games',
-    'friends',
-    'groups',
-  ];
-  const stringTypeKeys: string[] = [
-    '_id',
-    'name',
-    'email',
-    'username',
-    'type',
-    'winCondition',
-    'ownerId',
-    'theme',
-    'groupName',
-    'userId',
-    'message',
-    'password',
-    'avatar',
-  ];
+	const numberTypeKeys: string[] = ['wordSize'];
+	const objectArrayTypeKeys: string[] = ['boards'];
+	const stringArrayTypeKeys: string[] = [
+		'players',
+		'wordHistory',
+		'members',
+		'games',
+		'friends',
+		'groups',
+	];
+	const stringTypeKeys: string[] = [
+		'_id',
+		'name',
+		'email',
+		'username',
+		'type',
+		'winCondition',
+		'ownerId',
+		'theme',
+		'groupName',
+		'userId',
+		'message',
+		'password',
+		'avatar',
+	];
 
-  let res: string = '';
+	let res: string = '';
 
-  const bodyKeys = Object.keys(body);
-  let key: string = '';
+	const bodyKeys = Object.keys(body);
+	let key: string = '';
 
-  for (const [index, element] of Object.keys(bodyKeys).entries()) {
-    key = bodyKeys[index];
-    let reqType: string = '';
+	for (const [index, element] of Object.keys(bodyKeys).entries()) {
+		key = bodyKeys[index];
+		let reqType: string = '';
 
-    const valueToCheck: any = body[key as keyof typeof body];
-    const valueToCheckType = typeof valueToCheck;
+		const valueToCheck: any = body[key as keyof typeof body];
+		const valueToCheckType = typeof valueToCheck;
 
-    switch (true) {
-      case numberTypeKeys.includes(key): {
-        reqType = 'number';
+		switch (true) {
+			case numberTypeKeys.includes(key): {
+				reqType = 'number';
 
-        if (valueToCheckType !== reqType) {
-          res = createMessage(key, reqType, valueToCheckType);
-        }
-        break;
-      }
-      case objectArrayTypeKeys.includes(key): {
-        reqType = 'object[]';
-        const arrError = getArrayErrors('object', valueToCheck);
+				if (valueToCheckType !== reqType) {
+					res = createMessage(key, reqType, valueToCheckType);
+				}
+				break;
+			}
+			case objectArrayTypeKeys.includes(key): {
+				reqType = 'object[]';
+				const arrError = getArrayErrors('object', valueToCheck);
 
-        if (arrError) {
-          res = createMessage(key, reqType, arrError);
-        }
-        break;
-      }
-      case stringArrayTypeKeys.includes(key): {
-        reqType = 'string[]';
-        const arrError = getArrayErrors('string', valueToCheck);
+				if (arrError) {
+					res = createMessage(key, reqType, arrError);
+				}
+				break;
+			}
+			case stringArrayTypeKeys.includes(key): {
+				reqType = 'string[]';
+				const arrError = getArrayErrors('string', valueToCheck);
 
-        if (arrError) {
-          res = createMessage(key, reqType, arrError);
-        }
-        break;
-      }
-      case stringTypeKeys.includes(key): {
-        reqType = 'string';
+				if (arrError) {
+					res = createMessage(key, reqType, arrError);
+				}
+				break;
+			}
+			case stringTypeKeys.includes(key): {
+				reqType = 'string';
 
-        res =
-          valueToCheckType !== reqType
-            ? createMessage(key, reqType, valueToCheckType)
-            : !stringIsValid(valueToCheck, key)
-            ? `invalid characters for key: ${key}`
-            : res;
-        break;
-      }
-      default: {
-        break;
-      }
-    }
+				res =
+					valueToCheckType !== reqType
+						? createMessage(key, reqType, valueToCheckType)
+						: !stringIsValid(valueToCheck, key)
+						? `invalid characters for key: ${key}`
+						: res;
+				break;
+			}
+			default: {
+				break;
+			}
+		}
 
-    if (res) {
-      break;
-    }
-  }
+		if (res) {
+			break;
+		}
+	}
 
-  return res;
+	return res;
 };
 
 const processValidation = (
-  minMaxKeys: string,
-  body: object,
-  requiredKeys: string[],
-  allowedKeys?: string[]
+	minMaxKeys: string,
+	body: object,
+	requiredKeys: string[],
+	allowedKeys?: string[]
 ): string | null => {
-  let res: string | null = null;
-  const bodyKeys: string[] = Object.keys(body);
-  const bodyKeysLen: number = bodyKeys.length;
-  const minKeys: number = Number(minMaxKeys[0]);
+	let res: string | null = null;
+	const bodyKeys: string[] = Object.keys(body);
+	const bodyKeysLen: number = bodyKeys.length;
+	const minKeys: number = Number(minMaxKeys[0]);
 
-  if (bodyKeysLen < minKeys) {
-    return `Invalid Keys Length.  Expected: ${minMaxKeys}.  Received: ${bodyKeysLen}`;
-  }
+	if (bodyKeysLen < minKeys) {
+		return `Invalid Keys Length.  Expected: ${minMaxKeys}.  Received: ${bodyKeysLen}`;
+	}
 
-  const keysChecked = allowedKeys
-    ? checkKeys(bodyKeys, requiredKeys, allowedKeys)
-    : checkKeys(bodyKeys, requiredKeys);
+	const keysChecked = allowedKeys
+		? checkKeys(bodyKeys, requiredKeys, allowedKeys)
+		: checkKeys(bodyKeys, requiredKeys);
 
-  res = keysChecked
-    ? null
-    : `checkKey Error.  Invalid keys. Required Keys: [${requiredKeys.join(', ')}] Allowed Keys: [${
-        allowedKeys ? allowedKeys.join(', ') : null
-      }].  minMax: ${minMaxKeys}`;
+	res = keysChecked
+		? null
+		: `checkKey Error.  Invalid keys. Required Keys: [${requiredKeys.join(
+				', '
+		  )}] Allowed Keys: [${
+				allowedKeys ? allowedKeys.join(', ') : null
+		  }].  minMax: ${minMaxKeys}`;
 
-  if (!res) {
-    res = checkAllowedValueTypes(body);
-  }
+	if (!res) {
+		res = checkAllowedValueTypes(body);
+	}
 
-  return res;
+	return res;
 };
 
 export { processValidation };
