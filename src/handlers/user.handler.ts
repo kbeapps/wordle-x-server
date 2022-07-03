fab112cded86cf009ab57474efd975457759902d
lw3b3r <daevahush@gmail.com>
2022-07-03 16:32:34 -0400

    implemented structuring user payload data in user and auth handlers

--- src/handlers/user.handler.ts
+++ src/handlers/user.handler.ts
@@ -5,61 +5,79 @@ import utils from '../utils';
 
 const source: string = 'userMiddleware';
 
+const createUserPayload = (user: IUser): Partial<IUser> => {
+	return {
+		_id: user._id,
+		avatar: user.avatar,
+		email: user.email,
+		username: user.username,
+		friends: user.friends,
+		games: user.games,
+		groups: user.groups,
+	};
+};
+
 const get = async (req: Request, res: Response): Promise<void> => {
-  let status: number = 200;
-  let user: IUser = new User();
+	let status: number = 200;
+	let user: IUser = new User();
+	let payload;
 
-  try {
-    user = await controller.get({ [req.params.key]: req.params.value });
-  } catch (err) {
-    status = 500;
-    utils.errHandler(source, String(err));
-  }
+	try {
+		user = await controller.get({ [req.params.key]: req.params.value });
+		payload = createUserPayload(user);
+	} catch (err) {
+		status = 500;
+		utils.errHandler(source, String(err));
+	}
 
-  utils.responseHandler(res, status, 'getUser', user);
+	utils.responseHandler(res, status, 'getUser', payload);
 };
 
 const getById = async (req: Request, res: Response): Promise<void> => {
-  let status: number = 200;
-  let user: IUser = new User();
+	let status: number = 200;
+	let user: IUser = new User();
+	let payload;
 
-  try {
-    user = await controller.getById(req.cookies._id);
-  } catch (err) {
-    status = 500;
-    utils.errHandler(source, String(err));
-  }
+	try {
+		user = await controller.getById(req.cookies._id);
+		payload = createUserPayload(user);
+	} catch (err) {
+		status = 500;
+		utils.errHandler(source, String(err));
+	}
 
-  utils.responseHandler(res, status, 'getUser', user);
+	utils.responseHandler(res, status, 'getUser', payload);
 };
 
 const update = async (req: Request, res: Response): Promise<void> => {
-  let status: number = 200;
-  let user: IUser = new User();
+	let status: number = 200;
+	let user: IUser = new User();
+	let payload;
 
-  const query = { ...req.body };
+	const query = { ...req.body };
 
-  try {
-    user = await controller.update(req.cookies._id, query);
-  } catch (err) {
-    status = 500;
-    utils.errHandler(source, String(err));
-  }
+	try {
+		user = await controller.update(req.cookies._id, query);
+		payload = createUserPayload(user);
+	} catch (err) {
+		status = 500;
+		utils.errHandler(source, String(err));
+	}
 
-  utils.responseHandler(res, status, 'updateUser', user);
+	utils.responseHandler(res, status, 'updateUser', payload);
 };
 
 const remove = async (req: Request, res: Response): Promise<void> => {
-  let status: number = 200;
+	let status: number = 200;
 
-  try {
-    await controller.remove(req.cookies._id);
-  } catch (err) {
-    status = 500;
-    utils.errHandler(source, String(err));
-  }
+	try {
+		await controller.remove(req.cookies._id);
+	} catch (err) {
+		status = 500;
+		utils.errHandler(source, String(err));
+	}
 
-  utils.responseHandler(res, status, 'removeUser');
+	utils.responseHandler(res, status, 'removeUser');
 };
 
 export { get, getById, update, remove };
--- src/middleware/validators/cookies.validator.ts
+++ src/middleware/validators/cookies.validator.ts
@@ -1,12 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 import utils from '../../utils';
 
-const validateCookies = (req: Request, res: Response, next: NextFunction): void => {
-  if (!req.cookies._id) {
-    utils.responseHandler(res, 403, 'cookies', undefined, 'Invalid cookies.  Required: [ _id ]');
-    return;
-  }
-  next();
+const validateCookies = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): void => {
+	if (!req.cookies._id) {
+		utils.responseHandler(
+			res,
+			403,
+			'cookies',
+			undefined,
+			'Invalid cookies.  Required: [ _id ]'
+		);
+		return;
+	}
+	next();
 };
 
 export { validateCookies };
